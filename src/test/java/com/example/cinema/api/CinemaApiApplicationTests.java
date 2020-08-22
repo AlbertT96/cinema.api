2f2c3edd8fcefe6d86312f3fa8c6699faea25389
Albert <albert.traka@gmail.com>
2020-08-22 13:43:56 +0200

    Initial commit

---
+++ src/test/java/com/example/cinema/api/CinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinema.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
